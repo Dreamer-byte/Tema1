ac2df22e5b0df18e4b0840b0422a842a6717ea81
Dreamer-byte <parcestar@gmail.com>
2023-05-25 01:37:50 -0600

    Se resuleve el ejercicio 3 en el archivo Ejercicio3.py

---
+++ Ejercicio3.py
@@ -0,0 +1,8 @@
+b = float(input("Ingresa la base del triangulo: "))
+h = float(input("Ingresa la altura del triangulo: "))
+
+# Area de un tringulo base x altura entre 2
+
+a = b * h / 2
+
+print("El área del triangulo es: {}".format(a))
