7fad921c69eca0ce9e16bbb68d226f3c414891cf
Dreamer-byte <parcestar@gmail.com>
2023-05-25 01:21:29 -0600

    Se inicializa el control de versionses y se agrega el archivo Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+L = float(input("Ingresa un lado del cuadrado: "))
+#Calcular el área: Lado x Lado
+A = L * L
+
+#Calcular el perimetro: Lado x 4
+
+P = L * 4
+
+print(f"El area del cuadrado es: {A:.1f}")
+print(f"El Perímetro del cuadrado es: {P:.1f}")
